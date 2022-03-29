78353e11866a6cb61aed95fc953837cdf68e9eab
izharkevich <izharkevich@clevertec.ru>
2022-03-29 15:34:17 +0300

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import {Countries} from './components/Countries'
-import {Pagination} from './components/Pagination'
+import { Countries } from "./components/Countries";
+import { Pagination } from "./components/Pagination";
 
 function App() {
-  const [countries, setCountries] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
-  const [currentPage, setCurrentPage] = useState(1)
-  const [countriesPerPage] = useState(10)
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [countriesPerPage] = useState(10);
 
   useEffect(() => {
     const getCountries = async () => {
       try {
-        setIsLoading(true)
-        const response = await axios.get('https://restcountries.com/v3.1/all')
-        setCountries(response.data)
-        setIsLoading(false)
-      } catch (isError) {
-        setIsError(console.log(isError))
+        setIsLoading(true);
+        const response = await axios.get("https://restcountries.com/v3.1/all");
+        setCountries(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(console.log(error));
       }
-    }
-    getCountries()
-  }, [])
+    };
+    getCountries();
+  }, []);
 
-  const lastCoytryIndex = currentPage * countriesPerPage
-  const firstCoytryIndex = lastCoytryIndex - countriesPerPage
-  const currentCountry = countries.slice(firstCoytryIndex, lastCoytryIndex)
+  const lastCoytryIndex = currentPage * countriesPerPage;
+  const firstCoytryIndex = lastCoytryIndex - countriesPerPage;
+  const currentCountry = countries.slice(firstCoytryIndex, lastCoytryIndex);
+
+  const paginate = (numberPage) => setCurrentPage(numberPage);
+
+  const nextPage = () => setCurrentPage((prev) => prev + 1);
+  const prevPage = () => setCurrentPage((prev) => prev - 1);
 
   return (
     <div className="container mt-5">
@@ -36,9 +41,16 @@ function App() {
       <Pagination
         countriesPerPage={countriesPerPage}
         totalCountries={countries.length}
+        paginate={paginate}
       />
+      <button className="btn btn-primary" onClick={prevPage}>
+        Назад
+      </button>
+      <button className="btn btn-primary ms-2" onClick={nextPage}>
+        Вперед
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,19 +1,19 @@
-export const Pagination = ({countriesPerPage, totalCountries}) => {
-  const pageNumber = []
+export const Pagination = ({ countriesPerPage, totalCountries, paginate }) => {
+  const pageNumber = [];
   for (let i = 1; i <= Math.ceil(totalCountries / countriesPerPage); i++) {
-    pageNumber.push(i)
+    pageNumber.push(i);
   }
   return (
     <div>
       <ul className="pagination">
         {pageNumber.map((number) => (
           <li key={number} className="page-item">
-            <a href="!#" className="page-item">
+            <a href="!#" className="page-link" onClick={() => paginate(number)}>
               {number}
             </a>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
